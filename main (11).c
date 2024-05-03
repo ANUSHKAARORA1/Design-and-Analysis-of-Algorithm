f4f45794347e10283ec678860c8b32698a29b9d3
Anushka Arora <157234167+ANUSHKAARORA1@users.noreply.github.com>
2024-05-03 09:15:45 +0530

    Add files via upload

--- main (11).c
+++ main (11).c
@@ -1,63 +1,59 @@
-/******************************************************************************
-Given an unsorted array of alphabets containing duplicate elements. Design an algorithm
-and implement it using a program to find which alphabet has maximum number of
-occurrences andprint it. (Time Complexity = O(n)) (Hint: Use counting sort)
-Input Format:
-The first line contains number of
-test cases, T. For each test case,
-there will be two input lines. First
-line contains n (the size of array).
-Second line contains space-separated integers describing array.
-Output:
-The output will have T number of lines.
-For each test case, output will be the array element which has maximum occurrences and
-its total number of occurrences.
-If no duplicates are present (i.e. all the elements occur only once), output should be
-“No Duplicates Present”
-*******************************************************************************/
-#include <stdio.h>
-#include <string.h>
+/*Given a knapsack of maximum capacity w. N items are provided, each having its
+own value and weight. You have to Design an algorithm and implement it using a
+program to find the list of the selected items such that the final selected content has
+weight w and has maximum value. You can take fractions of items,i.e. the items can
+be broken into smaller pieces so that you have to carry
+only a fraction xi of item i, where 0 ≤xi≤ 1.*/
 
-void findMaxOccurrence(char arr[], int n) {
-    int count[26] = {0}; 
-    for (int i = 0; i < n; i++) {
-        if (arr[i] >= 'a' && arr[i] <= 'z') {
-            count[arr[i] - 'a']++;
-        }
-    }
-
-    int maxCount = 0;
-    char maxAlphabet = '\0';
-    for (int i = 0; i < 26; i++) {
-        if (count[i] > maxCount) {
-            maxCount = count[i];
-            maxAlphabet = 'a' + i;
-        }
-    }
-
-    if (maxCount > 1) {
-        printf("%c %d\n", maxAlphabet, maxCount);
-    } 
-    else 
-    {
-        printf("No Duplicates Present\n");
-    }
+[9:12 AM, 5/3/2024] Anushka: #include <stdio.h>
+#include <stdlib.h>
+struct Item {
+    int value;
+    int weight;
+    float valuePerWeight;
+};
+int compare(const void* a, const void* b) {
+    struct Item* item1 = (struct Item*)a;
+    struct Item* item2 = (struct Item*)b;
+    return (int)(item2->valuePerWeight - item1->valuePerWeight);
 }
-int main() 
-{
-    int T;
-    scanf("%d", &T);
-    while (T--) 
-    {
-        int n;
-        scanf("%d", &n);
-        char arr[n + 1]; 
-        for (int i = 0; i < n; i++) {
-            scanf(" %c", &arr[i]); 
+float fractionalKnapsack(struct Item* items, int n, int W) {
+    qsort(items, n, sizeof(items[0]), compare);
+
+    float totalValue = 0.0; 
+    int currentWeight = 0; 
+    
+    for (int i = 0; i < n; i++) {
+        if (currentWeight + items[i].weight <= W) {
+            totalValue += items[i].value;
+            currentWeight += items[i].weight;
+        } else {
+            float remainingWeight = W - currentWeight;
+            totalValue += items[i].valuePerWeight * remainingWeight;
+            break; 
         }
-        arr[n] = '\0'; 
-        findMaxOccurrence(arr, n);
     }
+    return totalValue;
+}
+int main() {
+    int n, W;
+    printf("Enter the number of items: ");
+    scanf("%d", &n);
+    printf("Enter the maximum capacity of the knapsack: ");
+    scanf("%d", &W);
+
+    struct Item* items = (struct Item*)malloc(n * sizeof(struct Item));
+    printf("Enter the value and weight of each item:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d %d", &items[i].value, &items[i].weight);
+        items[i].valuePerWeight = (float)items[i].value / items[i].weight;
+    }
+
+    float maxValue = fractionalKnapsack(items, n, W);
+
+    printf("Maximum value that can be obtained: %.2f\n", maxValue);
+
+    free(items);
 
     return 0;
 }
